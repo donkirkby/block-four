67ab608660d6bacc63948c7c756d8b4d73f38d13
Don Kirkby <donkirkby@gmail.com>
2017-11-17 21:53:57 -0800

    Click on spaces to capture them.
    Set the icon.

--- block_four.py
+++ block_four.py
@@ -1,4 +1,5 @@
-from itertools import accumulate
+from collections import namedtuple
+from random import choice
 
 import pygame
 
@@ -12,25 +13,34 @@ DIRECTIONS = {pygame.K_w: [0, -1],
               pygame.K_d: [1, 0]}
 
 
+WindowSize = namedtuple('WindowSize', 'grid_size line_width grid_x grid_y')
+
+
 class Game:
     def __init__(self, surface=None):
         pygame.init()
-        pygame.mixer.quit()
+        pygame.mixer.quit()  # Avoids high CPU.
 
         self.speed = [2, 2]
+        self.row_length = 9
+        self.spaces = [[0] * self.row_length for _ in range(self.row_length)]
+
+        self.background = (100, 100, 100)
+        self.grid_colour = (20, 20, 20)
+        self.player_colour = (100, 100, 250)
+        self.opponent_colour = (100, 200, 100)
 
         if surface is not None:
             self.surface = surface
             self.width, self.height = surface.get_size()
         else:
+            icon = create_icon(self.opponent_colour, self.player_colour)
+            pygame.display.set_icon(icon)
             self.width, self.height = 320, 240
             self.surface = pygame.display.set_mode((self.width, self.height),
                                                    pygame.RESIZABLE)
-        self.background = (100, 100, 100)
-        self.grid_colour = (20, 20, 20)
-        self.player_colour = (100, 100, 250)
-        self.opponent_colour = (100, 200, 100)
-        self.grid_size, self.line_width = self.rescale()
+
+        self.size = self.rescale()
         self.piece_shapes = (('**',
                               '**'),
                              ('***',
@@ -43,7 +53,11 @@ class Game:
                               ''))
 
     def rescale(self):
-        return min(self.width, self.height) * .5, self.width // 300
+        grid_size = min(self.width, self.height) * .5
+        return WindowSize(grid_size=grid_size,
+                          line_width=self.width // 300,
+                          grid_x=(self.width - grid_size) * .5,
+                          grid_y=(self.height - grid_size) * .5)
 
     def draw_grid_line(self, x1, y1, x2, y2):
         self.surface.fill(self.grid_colour,
@@ -51,43 +65,46 @@ class Game:
 
     def draw(self):
         self.surface.fill(self.background)
-        outer_x1 = (self.width - self.grid_size) * .5 - self.line_width
-        inner_x1 = outer_x1 + 2*self.line_width
-        inner_x2 = outer_x1 + self.grid_size
-        outer_x2 = inner_x2 + 2*self.line_width
-        outer_y1 = (self.height - self.grid_size) * .5 - self.line_width
-        inner_y1 = outer_y1 + 2*self.line_width
-        inner_y2 = outer_y1 + self.grid_size
-        outer_y2 = inner_y2 + 2*self.line_width
+        outer_x1 = self.size.grid_x - self.size.line_width
+        inner_x1 = outer_x1 + 2*self.size.line_width
+        inner_x2 = outer_x1 + self.size.grid_size
+        outer_x2 = inner_x2 + 2*self.size.line_width
+        outer_y1 = self.size.grid_y - self.size.line_width
+        inner_y1 = outer_y1 + 2*self.size.line_width
+        inner_y2 = outer_y1 + self.size.grid_size
+        outer_y2 = inner_y2 + 2*self.size.line_width
         self.draw_grid_line(outer_x1, outer_y1, outer_x2, inner_y1)
         self.draw_grid_line(outer_x1, inner_y2, outer_x2, outer_y2)
         self.draw_grid_line(outer_x1, inner_y1, inner_x1, outer_y2)
         self.draw_grid_line(inner_x2, inner_y1, outer_x2, outer_y2)
         for i in range(8):
-            y = (self.height - self.grid_size) * .5 + (i + 1) * self.grid_size / 9
-            line_width = self.line_width*2 if (i % 3) == 2 else self.line_width
+            y = self.size.grid_y + (i + 1) * self.size.grid_size / self.row_length
+            line_width = (self.size.line_width*2
+                          if (i % 3) == 2
+                          else self.size.line_width)
             pygame.draw.line(self.surface,
                              self.grid_colour,
-                             ((self.width - self.grid_size) * .5,
+                             (self.size.grid_x,
                               y),
-                             ((self.width + self.grid_size) * .5,
+                             ((self.width + self.size.grid_size) * .5,
                               y),
                              line_width)
-            x = (self.width - self.grid_size) * .5 + (i + 1) * self.grid_size / 9
+            x = self.size.grid_x + (i + 1) * self.size.grid_size / self.row_length
             pygame.draw.line(self.surface,
                              self.grid_colour,
                              (x,
-                              (self.height - self.grid_size) * .5),
+                              self.size.grid_y),
                              (x,
-                              (self.height + self.grid_size) * .5),
+                              (self.height + self.size.grid_size) * .5),
                              line_width)
 
-        self.draw_pieces2(outer_x1, outer_y2, self.player_colour, ydir=1)
-        self.draw_pieces2(outer_x1, outer_y1, self.opponent_colour, ydir=-1)
+        self.draw_pieces(outer_x1, outer_y2, self.player_colour, ydir=1)
+        self.draw_pieces(outer_x1, outer_y1, self.opponent_colour, ydir=-1)
+        self.draw_spaces()
 
-    def draw_pieces2(self, start_x, start_y, colour, ydir):
-        step_size = self.grid_size / 9
-        x = start_x + self.line_width - 5*step_size
+    def draw_pieces(self, start_x, start_y, colour, ydir):
+        step_size = self.size.grid_size / self.row_length
+        x = start_x + self.size.line_width - 5*step_size
         for shape in self.piece_shapes:
             y = start_y
             if ydir < 0:
@@ -104,53 +121,22 @@ class Game:
                                            step_size+1))
             x += step_size * (1 + max(len(row) for row in shape))
 
-    def draw_pieces(self, start_x, start_y, colour, ydir):
-        step_size = self.grid_size / 9
-        x = start_x + self.line_width - 5*step_size
-        self.draw_polygon(colour,
-                          (x, start_y + ydir*self.grid_size / 9),
-                          step_size,
-                          (0, 2*ydir),
-                          (2, 0),
-                          (0, -2*ydir))
-        x += 3*step_size
-        self.draw_polygon(colour,
-                          (x, start_y + ydir*self.grid_size / 9),
-                          step_size,
-                          (0, ydir),
-                          (1, 0),
-                          (0, ydir),
-                          (1, 0),
-                          (0, -ydir),
-                          (1, 0),
-                          (0, -ydir))
-        x += 4*step_size
-        self.draw_polygon(colour,
-                          (x, start_y + ydir*self.grid_size / 9),
-                          step_size,
-                          (0, ydir),
-                          (1, 0),
-                          (0, ydir),
-                          (2, 0),
-                          (0, -ydir),
-                          (-1, 0),
-                          (0, -ydir))
-        x += 4*step_size
-        self.draw_polygon(colour,
-                          (x, start_y + ydir*self.grid_size / 9),
-                          step_size,
-                          (0, ydir),
-                          (2, 0),
-                          (0, ydir),
-                          (1, 0),
-                          (0, -2*ydir))
-        x += 4*step_size
-        self.draw_polygon(colour,
-                          (x, start_y + ydir*self.grid_size / 9),
-                          step_size,
-                          (0, ydir),
-                          (4, 0),
-                          (0, -ydir))
+    def draw_spaces(self):
+        step_size = self.size.grid_size / self.row_length
+        radius = round(self.size.grid_size / 22)
+        for row in range(self.row_length):
+            for column in range(self.row_length):
+                player = self.get_player(row, column)
+                if player:
+                    x = round(self.size.grid_x + (column + 0.5)*step_size)
+                    y = round(self.size.grid_y + (row + 0.5)*step_size)
+                    colour = (self.player_colour
+                              if player == 1
+                              else self.opponent_colour)
+                    pygame.draw.circle(self.surface,
+                                       colour,
+                                       (x, y),
+                                       radius)
 
     def draw_polygon(self, colour, start_pos, step_size, *steps):
         pos = start_pos
@@ -162,6 +148,24 @@ class Game:
                             colour,
                             points)
 
+    def click(self, pos):
+        x, y = pos
+        step_size = self.size.grid_size / self.row_length
+        row = int((y - self.size.grid_y) // step_size)
+        column = int((x - self.size.grid_x) // step_size)
+        if row < 0 or self.row_length <= row:
+            return
+        if column < 0 or self.row_length <= column:
+            return
+        self.mark(row, column, player=1)
+        open_spaces = [(row, column)
+                       for row in range(self.row_length)
+                       for column in range(self.row_length)
+                       if not self.get_player(row, column)]
+        if open_spaces:
+            row, column = choice(open_spaces)
+            self.mark(row, column, player=-1)
+
     def main_loop(self):
         self.draw()
         pygame.display.flip()
@@ -171,21 +175,58 @@ class Game:
                     return
                 elif event.type == pygame.VIDEORESIZE:
                     self.width, self.height = event.size
-                    pygame.time.wait(100)
                     self.surface = pygame.display.set_mode((self.width, self.height),
                                                            pygame.RESIZABLE)
-                    self.grid_size, self.line_width = self.rescale()
+                    self.size = self.rescale()
                     pygame.display.update()
                     self.draw()
                     pygame.display.flip()
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    self.click(event.pos)
+                    self.draw()
+                    pygame.display.flip()
             pygame.time.wait(500)
+
+    def mark(self, row, column, player):
+        self.spaces[row][column] = player
+
+    def get_player(self, row, column):
+        return self.spaces[row][column]
+
+
+def create_icon(colour1, colour2):
+    surface = pygame.Surface((128, 128))
+    background = (100, 100, 100)
+    surface.fill(background)
+    pygame.draw.polygon(surface,
+                        colour1,
+                        ((4, 4),
+                         (4, 84),
+                         (44, 84),
+                         (44, 44),
+                         (124, 44),
+                         (124, 4)))
+    pygame.draw.polygon(surface,
+                        colour2,
+                        ((124, 124),
+                         (124, 44),
+                         (84, 44),
+                         (84, 84),
+                         (4, 84),
+                         (4, 124)))
+    return surface
 
 
 def live_main():
     surface = pygame.Surface((300, 200))
     try:
         game = Game(surface)
+        game.mark(0, 2, 1)
+        game.mark(4, 3, -1)
+        click_pos = (172, 112)
+        game.click(click_pos)
         game.draw()
+        pygame.draw.circle(surface, game.opponent_colour, click_pos, 2)
     except Exception as ex:
         surface.fill(BLACK)
         print(ex)
--- image_watcher.py
+++ image_watcher.py
@@ -4,7 +4,7 @@ If I tell the window to always stay on top, I can see it while I edit my code
 in live coding mode.
 """
 
-from argparse import ArgumentParser, FileType
+from argparse import ArgumentParser
 from pathlib import Path
 
 import pygame
@@ -16,10 +16,28 @@ def parse_args():
     return parser.parse_args()
 
 
+def create_icon():
+    handle = pygame.Surface((18, 90))
+    surface = pygame.Surface((128, 128))
+    background = (100, 100, 100)
+    foreground = (0, 0, 0)
+    handle.fill(background)
+    handle.fill(foreground, (3, 0, 12, 100))
+    handle = pygame.transform.rotate(handle, 45)
+    surface.fill(background)
+    size = 40
+    surface.blit(handle, (size+5, size+5))
+    pygame.draw.circle(surface, foreground, (size + 5, size + 5), size)
+    pygame.draw.circle(surface, background, (size + 5, size + 5), size - 10)
+    return surface
+
+
 def main():
     size = 50, 50
     args = parse_args()
     pygame.init()
+    pygame.mixer.quit()  # Avoids high CPU.
+    pygame.display.set_icon(create_icon())
     timestamp = None
     while True:
         for event in pygame.event.get():
